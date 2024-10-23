d43a8cccd0f89246c85ea31df4abbdb81f659b98
Matheus Castilho <85584099+cassmatheuss@users.noreply.github.com>
2024-10-23 11:13:48 -0300

    wip: start server working

---
+++ packages/backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Back-end da Jogoteca, online ✅')
+})
