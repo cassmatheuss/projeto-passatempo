71fe911aa7fa704b9d8a27a69e6168d89261aac3
cass <cassmatheuss@gmail.com>
2024-11-13 09:35:51 -0300

    feat: finished send email route

--- shared/sendEmail.utils.js
+++ shared/sendEmail.utils.js
@@ -19,7 +19,21 @@ const transporter = nodemailer.createTransport({
         to: data.email,
         subject: `Contato - ${data.nome_escola}`, 
         text: `Formulário preenchido por ${data.nome_escola}`,
-        html: "<b>Hello world?</b>",
+        html: `
+        <h1> ☀️ Preenchimento de formulário - ${data.nome_escola}</h1> <br />
+          <h2>Informações da Escola</h2> <br />
+            <b>Nome da escola - </b> <span>${data.nome_escola}</span> <br />
+            <b>Endereço - </b> <span>${data.endereco}</span> <br />
+            <b>Telefone - </b> <span>${data.telefone_escola}</span> <br />
+            <b>Email - </b> <span>${data.email_escola}</span> <br />
+          <h2>Informações do Representante</h2> <br />
+            <b>Nome do Representante - </b> <span>${data.nome_representante}</span> <br />
+            <b>Cargo do Representante - </b> <span>${data.cargo_representante}</span> <br />
+            <b>Telefone do Representante - </b> <span>${data.telefone_representante}</span> <br />
+            <b>Email do Representante- </b> <span>${data.email_representante}</span> <br />
+
+            
+        `,
     }, (error, info) => {
         if(error) {
             return console.log(error);
